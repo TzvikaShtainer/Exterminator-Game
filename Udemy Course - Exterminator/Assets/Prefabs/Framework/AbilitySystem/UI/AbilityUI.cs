eb03e6a6d7ae90914766b5143c7e302c51c4221b
TzvikaShtainer <TzvikaShtainer@gmail.com>
2024-01-31 07:55:24 +0200

    Android Build Successful

--- Udemy Course - Exterminator/Assets/Prefabs/Framework/AbilitySystem/UI/AbilityUI.cs
+++ Udemy Course - Exterminator/Assets/Prefabs/Framework/AbilitySystem/UI/AbilityUI.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections;
-using Microsoft.Unity.VisualStudio.Editor;
+//using Microsoft.Unity.VisualStudio.Editor;
 using Unity.VisualScripting;
 using UnityEngine;
 using Image = UnityEngine.UI.Image;
